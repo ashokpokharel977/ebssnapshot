bc47202c94ce0bf09895f80e2acc2bd82e898695
ashok pokharel <ashokpokharel977@gmail.com>
2019-03-03 15:11:41 +0545

    fix 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var volumeParams = {
 	]
 };
 
-var volumeid = ec2.describeVolumes(volumeParams, function(err, data, volumeid) {
+var volumeid = ec2.describeVolumes(volumeParams, function(err, data) {
 	if (err) console.log(err, err.stack);
 	else {
 		// an error occurred
