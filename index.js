0e90ddb90027cf372ea68ab0d6247b351efb404c
ashok pokharel <ashokpokharel977@gmail.com>
2019-03-03 14:53:39 +0545

    describeVolumes

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ var AWS = require('aws-sdk');
 AWS.config.region = 'us-east-1';
 AWS.config.credentials = new AWS.EC2MetadataCredentials();
 var meta = new AWS.MetadataService();
-meta.request('/latest/meta-data/block-device-mapping', function(err, data) {
-	console.log(data);
-	console.log(err);
-});
 
 // Create EC2 service object
-// var ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+var ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+var volumeParams = {
+	Filters: [
+		{
+			Name: 'tag:Owner',
+			Values: [ 'ashok' ]
+		}
+	]
+};
 
-// var params = {
-// 	Description: 'This is my root volume snapshot.',
-// 	VolumeId: 'vol-05a7e7be288c34e7a'
-// };
+ec2.describeVolumes(volumeParams, function() {
+	if (err)
+		console.log(err, err.stack); // an error occurred
+	else console.log(data); // successful response
+});
+
+var params = {
+	Description: 'This is my root volume snapshot.',
+	VolumeId: 'vol-05a7e7be288c34e7a'
+};
 // ec2.createSnapshot(params, function(err, data) {
 // 	if (err)
 // 		console.log(err, err.stack); // an error occurred
