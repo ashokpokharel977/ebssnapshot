2138f57a130f0a844c750824ae023d3d9b19d8ec
ashok pokharel <ashokpokharel977@gmail.com>
2019-03-03 10:52:52 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Load credentials and set region from JSON file
+//AWS.config.loadFromPath('./config.json');
+
+// Create EC2 service object
+var ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+
+var params = {
+	Description: 'This is my root volume snapshot.',
+	VolumeId: 'vol-1234567890abcdef0'
+};
+ec2.createSnapshot(params, function(err, data) {
+	if (err)
+		console.log(err, err.stack); // an error occurred
+	else console.log(data); // successful response
+
+	data = {
+		Description: 'This is my root volume snapshot.',
+		OwnerId: '012345678910',
+		SnapshotId: 'snap-066877671789bd71b',
+		State: 'pending',
+		Tags: [],
+		VolumeId: 'vol-1234567890abcdef0',
+		VolumeSize: 20
+	};
+});
