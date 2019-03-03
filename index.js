546b5faccd81300e029987e49115e5d7f82b8565
ashok pokharel <ashokpokharel977@gmail.com>
2019-03-03 17:19:55 +0545

    fix 15

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var AWS = require('aws-sdk');
 //AWS.config.loadFromPath('./config.json');
 AWS.config.region = 'us-east-1';
 AWS.config.credentials = new AWS.EC2MetadataCredentials();
-var meta = new AWS.MetadataService();
-
 // Create EC2 service object
 var ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
 var volumeParams = {
@@ -16,30 +14,33 @@ var volumeParams = {
 		}
 	]
 };
-var volumeid = '';
-ec2.describeVolumes(volumeParams, (err, data) => {
-	if (err) console.log(err, err.stack);
-	else {
-		console.log(data.Volumes[0].VolumeId);
-	}
-	global.volumeid = data.Volumes[0].VolumeId;
-});
-console.log(volumeid);
-// var params = {
-// 	Description: 'This is my root volume snapshot.',
-// 	VolumeId: volumeid
-// };
-// ec2.createSnapshot(params, function(err, data) {
-// 	if (err)
-// 		console.log(err, err.stack); // an error occurred
-// 	else console.log(data); // successful response
+function getVolumeId() {
+	var id = '';
+	ec2.describeVolumes(volumeParams, (err, data) => {
+		if (err) console.log(err, err.stack);
+		else {
+			console.log(data.Volumes[0].VolumeId);
+			id = data.Volumes[0].VolumeId;
+		}
+	});
+	return id;
+}
 
-// 	data = {
-// 		Description: 'This is my root volume snapshot.',
-// 		SnapshotId: 'snap-066877671789bd71b',
-// 		State: 'pending',
-// 		Tags: [],
-// 		VolumeId: 'vol-05a7e7be288c34e7a',
-// 		VolumeSize: 20
-// 	};
-// });
+var params = {
+	Description: 'This is my root volume snapshot.',
+	VolumeId: getVolumeId()
+};
+ec2.createSnapshot(params, function(err, data) {
+	if (err)
+		console.log(err, err.stack); // an error occurred
+	else console.log(data); // successful response
+
+	data = {
+		Description: 'This is my root volume snapshot.',
+		SnapshotId: 'snap-066877671789bd71b',
+		State: 'pending',
+		Tags: [],
+		VolumeId: 'vol-05a7e7be288c34e7a',
+		VolumeSize: 20
+	};
+});
