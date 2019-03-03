310309a51b33598c26194bc91b7215eaf90adb11
ashok pokharel <ashokpokharel977@gmail.com>
2019-03-03 12:39:55 +0545

    check ony device mappling

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ meta.request('/latest/meta-data/block-device-mapping/', function(err, data) {
 });
 
 // Create EC2 service object
-var ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+// var ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
 
-var params = {
-	Description: 'This is my root volume snapshot.',
-	VolumeId: 'vol-05a7e7be288c34e7a'
-};
-ec2.createSnapshot(params, function(err, data) {
-	if (err)
-		console.log(err, err.stack); // an error occurred
-	else console.log(data); // successful response
+// var params = {
+// 	Description: 'This is my root volume snapshot.',
+// 	VolumeId: 'vol-05a7e7be288c34e7a'
+// };
+// ec2.createSnapshot(params, function(err, data) {
+// 	if (err)
+// 		console.log(err, err.stack); // an error occurred
+// 	else console.log(data); // successful response
 
-	data = {
-		Description: 'This is my root volume snapshot.',
-		SnapshotId: 'snap-066877671789bd71b',
-		State: 'pending',
-		Tags: [],
-		VolumeId: 'vol-05a7e7be288c34e7a',
-		VolumeSize: 20
-	};
-});
+// 	data = {
+// 		Description: 'This is my root volume snapshot.',
+// 		SnapshotId: 'snap-066877671789bd71b',
+// 		State: 'pending',
+// 		Tags: [],
+// 		VolumeId: 'vol-05a7e7be288c34e7a',
+// 		VolumeSize: 20
+// 	};
+// });
